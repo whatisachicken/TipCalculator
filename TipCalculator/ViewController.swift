da3521479ab91c35f40249635ec8561461cf676f
Jonathan <effervescent@Jonathans-iMac.local>
2019-01-26 11:30:33 -0800

    setup the basic layout

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,19 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     //Reference controls
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     
-    @IBOutlet weak var lblBill: UILabel!
-    
-    @IBOutlet weak var txtSubtotal: UITextField!
-    
-    @IBOutlet weak var lblTipAmnt: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-    @IBAction func readAmnt(_ sender: Any) {
-        lblTipAmnt.text = txtSubtotal.text
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercents = [0.15, 0.2, 0.25]
+        //Get bill amount from text field
+        let bill = Double(billTextField.text!) ?? 0
+        //Calculate tip and add to total
+        let tip = bill * tipPercents[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //Update the total
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 }
 
